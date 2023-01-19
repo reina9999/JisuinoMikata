862307f23a7b2e0d2c648dd491aa252fed3583de
ReinaYamamoto <112670391+reina9999@users.noreply.github.com>
2023-01-19 12:05:19 +0000

    Fix

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -36,7 +36,7 @@ class Public::CustomersController < ApplicationController
     end
   end
 
-  def withdraw
+  def withdraw　#会員無効
     # is_deletedカラムをtrueに変更することにより削除フラグを立てる
     current_customer.update(is_deleted: true)
     reset_session
