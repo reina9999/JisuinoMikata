5dd3b29c9b46a420767de89a4bd022b2ce59af70
ReinaYamamoto <112670391+reina9999@users.noreply.github.com>
2023-01-21 09:11:17 +0000

    Soat

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,6 +4,18 @@ class Public::SearchesController < ApplicationController
    @range = params[:range] #検索モデル
    @content = params[:word] #検索ワード
    @model = "cook"
-   @cooks = Cook.looks(params[:search], params[:word]).order("created_at DESC").page(params[:page]).per(8) #降順に指定
+  # @cooks = Cook.looks(params[:search], params[:word]).order("created_at DESC").page(params[:page]).per(8) #降順に指定
+
+
+   #ソート機能
+    if params[:latest]
+      @cooks = Cook.looks(params[:search], params[:word]).latest.page(params[:page]).per(8)
+    elsif params[:old]
+      @cooks = Cook.looks(params[:search], params[:word]).old.page(params[:page]).per(8)
+    elsif params[:star_count]
+      @cooks = Cook.looks(params[:search], params[:word]).star_count.page(params[:page]).per(8)
+    else
+      @cooks = Cook.looks(params[:search], params[:word]).order("created_at DESC").page(params[:page]).per(8) #降順に指定
+    end
   end
 end
